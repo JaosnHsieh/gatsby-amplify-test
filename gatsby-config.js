340da0840757ffc6b3d070f0166877080c7d035b
JaosnHsieh <appleoxxo@gmail.com>
2020-03-24 17:57:57 +0800

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,43 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `qq mother fucker`,
+    description: `A simple description about pandas eating lots...`,
+    author: `gatsbyjs`,
+  },
+
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+  ],
 }
